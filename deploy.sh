c135dab11af01102c4af8fb15f8f325c30a35505
YooDing <coding1618@gmail.com>
2019-07-13 19:02:43 +0800

    new deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,9 @@
 version="kmall-0.0.1-SNAPSHOT"
 warName=${version}.war
 echo -e "\033[32m -----------------------------------------------------------------------------------------------\033[0m"
-echo -e "  [ \033[32mINFO\033[0m ]   deploy war to Tomcat ....."
+echo -e "  [ \033[32mINFO\033[0m ]   start deploy war to Tomcat....."
+echo -e "  [ \033[32mINFO\033[0m ]   this shell by YooDing Email:coding1618@gmail.com"
+echo -e "\033[32m -----------------------------------------------------------------------------------------------\033[0m"
 wget -c http://jenkins.codegc.me/${warName}
 mv ./${warName} /root/apache-tomcat-9.0.19/webapps/ROOT
 cd /root/apache-tomcat-9.0.19/webapps/ROOT
