6403e4dd01d8f4daa789b78befb513b55357d586
YooDing <coding1618@gmail.com>
2019-07-13 16:57:57 +0800

    no message

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#YooDing编写脚本 qq2420498526
 version="kmall-0.0.1-SNAPSHOT"
 warName=${version}.war
 echo -e "\033[32m -----------------------------------------------------------------------------------------------\033[0m"
@@ -8,5 +9,7 @@ mv ./${warName} /root/apache-tomcat-9.0.19/webapps/ROOT
 cd /root/apache-tomcat-9.0.19/webapps/ROOT
 jar -xvf ${warName}
 rm -rf ${warName}
+echo -e "\033[32m -----------------------------------------------------------------------------------------------\033[0m"
 echo -e "  [ \033[32mINFO\033[0m ]   deploy war to Tomcat Done."
 echo -e "\033[32m -----------------------------------------------------------------------------------------------\033[0m"
+exit
