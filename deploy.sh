5695968782d77ae43af0cd81f4c58891ca981c25
YooDing <2420498526@qq.com>
2019-07-13 21:04:36 +0800

    add restart

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@
 version="kmall-0.0.1-SNAPSHOT"
 warName=${version}.war
 echo -e "\033[32m -----------------------------------------------------------------------------------------------\033[0m"
-echo -e "  [ \033[32mINFO\033[0m ]   start deploy war to Tomcat....."
+echo -e "  [ \033[32mINFO\033[0m ]   start deploy war to tomcat....."
 echo -e "  [ \033[32mINFO\033[0m ]   this shell by YooDing Email:coding1618@gmail.com"
 echo -e "\033[32m -----------------------------------------------------------------------------------------------\033[0m"
 wget -c http://jenkins.codegc.me/${warName}
@@ -11,7 +11,14 @@ mv ./${warName} /root/apache-tomcat-9.0.19/webapps/ROOT
 cd /root/apache-tomcat-9.0.19/webapps/ROOT
 jar -xvf ${warName}
 rm -rf ${warName}
+sh /root/apache-tomcat-9.0.19/bin/shutdown.sh
+slepp 5
+sh /root/apache-tomcat-9.0.19/bin/startup.sh
 echo -e "\033[32m -----------------------------------------------------------------------------------------------\033[0m"
-echo -e "  [ \033[32mINFO\033[0m ]   deploy war to Tomcat Done."
+echo -e "  [ \033[32mINFO\033[0m ]   restart tomcat 20s...."
+echo -e "\033[32m -----------------------------------------------------------------------------------------------\033[0m"
+sleep 20
+echo -e "\033[32m -----------------------------------------------------------------------------------------------\033[0m"
+echo -e "  [ \033[32mINFO\033[0m ]   deploy war to tomcat Done."
 echo -e "\033[32m -----------------------------------------------------------------------------------------------\033[0m"
 exit
